9c3d25e4495482d84ec243ef6083290dede594ae
whywefight <30388756+whywefight@users.noreply.github.com>
2018-04-20 21:34:43 +0200

    Create prepare.sh

---
+++ prepare.sh
@@ -0,0 +1,9 @@
+sudo apt-get update -y
+sleep 3
+sudo apt-get upgrade -y
+sleep 3
+sudo apt-get dist-upgrade -y
+sleep 3
+sudo wget https://raw.githubusercontent.com/whywefight/bwk/master/run.sh
+sudo chmod 777 run.sh
+sudo reboot
